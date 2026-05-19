574ecf0f75a4565d618c75adeb0ef4f056fe9a87
DanielBarbaranGarcia <daniel1717bg@gmail.com>
2026-05-19 14:11:45 -0500

    a

--- app/views/layouts/footer.php
+++
@@ -1,4 +0,0 @@
-<hr>
-<script src="<?= URLROOT ?>/js/app.js"></script>
-</body>
-</html>
--- app/views/layouts/header.php
+++
@@ -1,9 +0,0 @@
-<!DOCTYPE html>
-<html>
-<head>
-<title>Sistema Asistencia</title>
-<link rel="stylesheet" href="<?= URLROOT ?>/css/estilos.css">
-</head>
-<body>
-<h1>SISTEMA DE ASISTENCIA</h1>
-<hr>
